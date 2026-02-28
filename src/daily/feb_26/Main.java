94c942608407b3b16c7f6fa86fa302a6883731e0
Pulkit Shringi <pulkitshringi02@gmail.com>
2026-02-28 19:04:44 +0530

    updated

--- src/daily/feb_26/Main.java
+++ src/daily/feb_26/Main.java
@@ -1,16 +1,7 @@
 package daily.feb_26;
 
- class Another{
-    private int no;  // private
-    public void func(int no){
-        this.no = no+10;
-        System.out.println(this.no);
-    }
-}
-
 public class Main{
     public static void main(String[] args) {
-        Another a1 = new Another();
-        a1.func(42);
+        System.out.println("System hila denge");
     }
 }
