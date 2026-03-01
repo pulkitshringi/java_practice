26a3330e348d94de260656532366dba319bd7e7f
Pulkit Shringi <pulkitshringi02@gmail.com>
2026-03-01 19:26:29 +0530

    practiced java ques

---
+++ src/daily/Main.java
@@ -0,0 +1,7 @@
+package daily;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("check");
+    }
+}
--- src/daily/feb_26/Main.java
+++
@@ -1,7 +0,0 @@
-package daily.feb_26;
-
-public class Main{
-    public static void main(String[] args) {
-        System.out.println("System hila denge");
-    }
-}
