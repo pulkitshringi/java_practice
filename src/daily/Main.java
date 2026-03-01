8c78c3e21d992e19080e1612bebfd8c9d403c235
Pulkit Shringi <pulkitshringi02@gmail.com>
2026-03-02 00:09:52 +0530

    practiced ques

--- src/daily/Main.java
+++ src/daily/Main.java
@@ -1,7 +1,19 @@
 package daily;
+class A{
+    String name;
+    int age;
+    
+    @Override
+    public String toString(){
+        return "Name : " + name + "\nAge: " + age;
+    }
+}
 
-public class Main {
+public class Main{
     public static void main(String[] args) {
-        System.out.println("check");
+        A obj = new A();
+        obj.name = "pooki";
+        obj.age = 23;
+        System.out.println(obj);
     }
 }
