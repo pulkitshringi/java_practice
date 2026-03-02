43dd57d3d925e59df68211c92726d94f3776d7e7
Pulkit Shringi <pulkitshringi02@gmail.com>
2026-03-02 19:05:28 +0530

    practiced java

--- src/daily/Main.java
+++ src/daily/Main.java
@@ -1,19 +1,48 @@
 package daily;
+
+import java.util.Objects;
+
 class A{
     String name;
     int age;
-    
+
     @Override
-    public String toString(){
-        return "Name : " + name + "\nAge: " + age;
+    public int hashCode() {
+        int hash = 7;
+        hash = 47 * hash + Objects.hashCode(this.name);
+        hash = 47 * hash + this.age;
+        return hash;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final A other = (A) obj;
+        if (this.age != other.age) {
+            return false;
+        }
+        return Objects.equals(this.name, other.name);
+    }
+
+    
 }
 
 public class Main{
     public static void main(String[] args) {
         A obj = new A();
-        obj.name = "pooki";
+        obj.name = "mitu";
         obj.age = 23;
-        System.out.println(obj);
+        A obj2 = new A();
+        obj2.name = "pooki";
+        obj2.age = 23;
+        System.out.println(obj.equals(obj2)); 
     }
 }
