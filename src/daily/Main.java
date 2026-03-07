b0f0b2f4dbacd2a595633b54fc4da6927daf49fb
Pulkit Shringi <pulkitshringi02@gmail.com>
2026-03-07 15:52:34 +0530

    practiced java

--- src/daily/Main.java
+++ src/daily/Main.java
@@ -1,21 +1,6 @@
 package daily;
 
-class A{
-    void show1(){
-        System.out.println("show method from A");
-    }
-}
-
-class B extends A{
-    void show2(){
-        System.out.println("show method from B");
-    }
-}
-
 public class Main{
     public static void main(String[] args) {
-        A obj = new B();  
-        B obj2 = (B) obj;
-        obj2.show1();
-    }
-}
+        System.out.println("no stopping");
+    }}
