a673c2de50c672b35169416d089b2a9cf69decae
Pulkit Shringi <pulkitshringi02@gmail.com>
2026-03-14 15:55:25 +0530

    practiced exceptions

---
+++ src/fundamentals/exceptions/Main.java
@@ -0,0 +1,11 @@
+package fundamentals.exceptions;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hii pooki");
+        int i = 0;
+        System.out.println(10/i);
+        System.out.println("bye pooki");
+    }
+}
+
