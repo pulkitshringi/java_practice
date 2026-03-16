61f003c1c963775c415d5f85c2caa7b1e16ef816
Pulkit Shringi <pulkitshringi02@gmail.com>
2026-03-16 21:40:56 +0530

    practiced java ques

--- src/daily/Main.java
+++ src/daily/Main.java
@@ -1,26 +1,8 @@
 package daily;
 
-import java.util.Arrays;
-
 class Main{
     public static void main(String[] args){
-        int[] arr = new int[3];
-        int j = 0;
-        int i;
-        try {
-            arr[3] = 1;
-        } 
-        catch(ArrayIndexOutOfBoundsException e){
-            System.out.println("you are accessing out of index :)");
-            System.out.println(e);
-        }
-        try{            
-            i = 10/j;
-        }
-        catch(ArithmeticException e){
-            System.out.println("dividing by zero :/");
-            System.out.println(e);
-        }
-        System.out.println(Arrays.toString(arr));
+        int j = 10 / 0;
+        System.out.println(j);
     }
 }
