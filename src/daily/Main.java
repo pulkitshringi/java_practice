718d2c066ec69bed7b0ef35db15fc29ef99b32dc
Pulkit Shringi <pulkitshringi02@gmail.com>
2026-03-17 21:22:33 +0530

    practiced java

--- src/daily/Main.java
+++ src/daily/Main.java
@@ -1,8 +1,30 @@
 package daily;
+import java.io.FileReader;
 
+class Other{
+    public void d() throws ClassNotFoundException{
+        Class.forName("daily.Main"); // no error as Class 'Main' exists
+    }
+    public void e() throws ClassNotFoundException{
+        Class.forName("daily.Animal"); // error as class does not exist
+    }
+}
 class Main{
     public static void main(String[] args){
-        int j = 10 / 0;
-        System.out.println(j);
+
+        Other obj = new Other();
+        try {
+            obj.d();
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        try {
+            obj.e();
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        System.out.println("last statement");
     }
 }
