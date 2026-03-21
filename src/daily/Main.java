13991bd671c201fe0d240e527f80d4fcf06e738d
Pulkit Shringi <pulkitshringi02@gmail.com>
2026-03-21 19:09:30 +0530

    practiced java

--- src/daily/Main.java
+++ src/daily/Main.java
@@ -1,25 +1,34 @@
 package daily;
 
-class A extends Thread{
-    public void run(){
-        for (int i = 0; i < 10; i++) {
-            System.out.println("hii");
-        }
-    }
-}
-class B extends Thread{
-    public void run(){
-        for (int i = 0; i < 10; i++) {
-            System.out.println("hello");
-        }
-    }
-}
+class Main {
 
-class Main{
-    public static void main(String[] args){
-            A obj1 = new A();
-            B obj2 = new B();
-            obj1.start();
-            obj2.start();
-        } 
+    public static void main(String[] args) {
+
+        Runnable obj1 = () -> {
+                for (int i = 0; i < 10; i++) {
+                    System.out.println("hii");
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+        };
+        Runnable obj2 = () -> {
+                for (int i = 0; i < 10; i++) {
+                    System.out.println("hello");
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+        };
+        Thread t1 = new Thread(obj1);
+        Thread t2 = new Thread(obj2);
+        t1.start();
+        t2.start();
     }
+}
