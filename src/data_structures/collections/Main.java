acedd24e61826deb29673ac3ea5bb4f4d9595fcd
Pulkit Shringi <pulkitshringi02@gmail.com>
2026-03-24 20:51:07 +0530

    practiced java

--- src/data_structures/collections/Main.java
+++ src/data_structures/collections/Main.java
@@ -1,16 +1,17 @@
 package data_structures.collections;
 import java.util.*;
 
-
 public class Main {
     public static void main(String[] args) {
-        Map<String,Integer> mp = new HashMap<>();
-        mp.put("pooki",100);
-        mp.put("mitu", 101);
-        mp.put("sohpy", 200);
-        mp.put("pooki", 42); 
-        for(Map.Entry<String, Integer> entry: mp.entrySet()){
-            System.out.println(entry.getKey() + " " + entry.getValue());
-        }
-    };
-}
+        List<Integer> obj = new ArrayList<>();
+            obj.add(42);
+            obj.add(16);
+            obj.add(53);
+            obj.add(19);
+            Comparator<Integer> cmp = (no1,no2) -> no1%10<no2%10?-1:1;
+            Collections.sort(obj,cmp); // passing it here
+            System.out.println(obj);
+            };  
+        };
+
+        
