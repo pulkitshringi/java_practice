34a4b27f99b965a4196cd31c49c5d8b5823212b2
Pulkit Shringi <pulkitshringi02@gmail.com>
2026-03-25 21:48:08 +0530

    practiced java

--- src/daily/Main.java
+++ src/daily/Main.java
@@ -1,37 +1,39 @@
 package daily;
+import java.lang.reflect.Array;
+import java.util.*;
 
-class Counter{
-    int counter = 0;
-    public synchronized void increment(){
-        counter++;
+class Human{
+    void sleep(){
+        System.out.println("Human sleeps.");
+    }
+}
+
+class Employee extends Human{
+    @Override
+    void sleep(){
+        System.out.println("Employee sleeps.");
+    }
+}
+class Developer extends Employee{
+    @Override
+    void sleep(){
+        System.out.println("Developer sleeps.");
     }
 }
 
 class Main {
 
     public static void main(String[] args) {
-        Counter cnt = new Counter();
-        Runnable obj1 = () ->{
-            for (int i = 0; i < 1000; i++) {
-                cnt.increment();
-            }
-        };
-
-        Runnable obj2 = () ->{
-            for (int i = 0; i < 1000; i++) {
-                cnt.increment();
-            }
-        };
-        Thread t1 = new Thread(obj1); 
-        Thread t2 = new Thread(obj2);
-        t1.start();
-        t2.start();
-        try{
-        t1.join();  // waiting for thread 1 to complete
-        t2.join(); // waiting for thread 2 complete
-        }catch(Exception e){
-
-        };
-        System.out.println(cnt.counter);
+        List<Human> h1 = new ArrayList<>(Arrays.asList(new Human()));
+        List<Employee> e1 = new ArrayList<>(Arrays.asList(new Employee()));
+        List<Developer> d1 = new ArrayList<>(Arrays.asList(new Developer()));
+            func(h1);
+            func(e1);
+            func(d1);
     }
-}
+    public static void func(List<? extends Human> l1){
+        for(Human obj: l1){
+            obj.sleep();
+        }
+    }
+};
--- src/data_structures/collections/Main.java
+++ src/data_structures/collections/Main.java
@@ -1,17 +1,32 @@
 package data_structures.collections;
 import java.util.*;
 
+class Student{
+    String name;
+    int age;
+    public Student(String name, int age){
+        this.name = name;
+        this.age = age;
+    };
+
+    @Override
+    public String toString(){ 
+        return "Student name is " + name + " and age is " + age;
+    }
+}
+
+
 public class Main {
     public static void main(String[] args) {
-        List<Integer> obj = new ArrayList<>();
-            obj.add(42);
-            obj.add(16);
-            obj.add(53);
-            obj.add(19);
-            Comparator<Integer> cmp = (no1,no2) -> no1%10<no2%10?-1:1;
-            Collections.sort(obj,cmp); // passing it here
+        List<Student> l1 = new ArrayList<>();
+        l1.add(new Student("pooki", 23));
+        l1.add(new Student("mitu", 21));
+        l1.add(new Student("Chad", 69));
+        Comparator<Student> cmp = (obj1, obj2) -> obj1.age - obj2.age;
+        Collections.sort(l1, cmp);
+        for(Student obj: l1){
             System.out.println(obj);
+        }
             };  
         };
 
-        
