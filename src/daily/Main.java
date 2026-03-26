15e9a327ad0656d531368a9529336e8c27b2ff62
Pulkit Shringi <pulkitshringi02@gmail.com>
2026-03-26 22:11:52 +0530

    practiced java

--- src/daily/Main.java
+++ src/daily/Main.java
@@ -1,39 +1,12 @@
 package daily;
-import java.lang.reflect.Array;
-import java.util.*;
-
-class Human{
-    void sleep(){
-        System.out.println("Human sleeps.");
-    }
-}
-
-class Employee extends Human{
-    @Override
-    void sleep(){
-        System.out.println("Employee sleeps.");
-    }
-}
-class Developer extends Employee{
-    @Override
-    void sleep(){
-        System.out.println("Developer sleeps.");
-    }
-}
+import java.time.*;
 
 class Main {
-
     public static void main(String[] args) {
-        List<Human> h1 = new ArrayList<>(Arrays.asList(new Human()));
-        List<Employee> e1 = new ArrayList<>(Arrays.asList(new Employee()));
-        List<Developer> d1 = new ArrayList<>(Arrays.asList(new Developer()));
-            func(h1);
-            func(e1);
-            func(d1);
-    }
-    public static void func(List<? extends Human> l1){
-        for(Human obj: l1){
-            obj.sleep();
-        }
+        LocalTime time = LocalTime.now();
+        System.out.println(time.getHour());
+        System.out.println(time.getMinute());
+        System.out.println(time.getSecond());
+        System.out.println(time.getNano());
     }
 };
