a59177586a92a75d444db07d670d393022032a9b
Pulkit Shringi <pulkitshringi02@gmail.com>
2026-04-19 15:20:50 +0530

    practiced java

--- src/daily/Main.java
+++ src/daily/Main.java
@@ -2,19 +2,27 @@ package daily;
 import java.io.*;
 import java.util.Arrays;
 
-class Main {
-    public static void main(String[] args) throws IOException {
-            File file1 = new File("src/daily/pooki.txt");
-            FileWriter writer = new FileWriter(file1);
-            try{
-            writer.write("I have overriden the text :) \n");
-            writer.write("I'm in new line hehe\n"); 
-            writer.write(97); 
-            }
-            catch(Exception e){}
-            finally{
-                writer.close(); // always close it at end 
-            };
+class Human implements Serializable{
+    String name;
+    transient int age; // won't be sent to file
 
+    public Human(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString(){
+        return "name = " + name + " age = " + age;
+    }
+}
+
+class Main {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+            FileInputStream fis = new FileInputStream("src/daily/pooki.txt");
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            Human h1 = (Human) ois.readObject();
+            System.out.println(h1);
+            ois.close(); 
        }    
 };
